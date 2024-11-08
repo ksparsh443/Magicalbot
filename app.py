9a07ce5667a222b9518f5f643bc55af9bba39e92
Sparsh kandpal <ksparsh443@gmail.com>
2024-11-08 11:14:15 +0530

    api commit

--- app.py
+++ app.py
@@ -71,17 +71,19 @@ with st.sidebar:
 
     st.subheader('Models and parameters')
     selected_model = st.selectbox('Choose a Llama2 model', [
-                                  'Llama2-7B', 'Llama2-13B'], key='selected_model')
+                                  'Llama2-7B', 'Llama2-13B', 'Llama3-70b'], key='selected_model')
     if selected_model == 'Llama2-7B':
         llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
     elif selected_model == 'Llama2-13B':
         llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
+    elif selected_model == 'Llama3-70B':
+        llm = 'meta/meta-llama-3-70b-instruct'
     temperature = st.sidebar.slider(
         'temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
     top_p = st.sidebar.slider('top_p', min_value=0.01,
                               max_value=1.0, value=0.9, step=0.01)
     max_length = st.sidebar.slider(
-        'max_length', min_value=32, max_value=128, value=120, step=8)
+        'max_length', min_value=32, max_value=256, value=120, step=8)
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
